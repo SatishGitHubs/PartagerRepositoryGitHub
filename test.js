759c52028c0af942cb0bb96e0761266ad8d566ac
SatishGitHubs <SatishGitHubs@users.noreply.github.com>
2015-11-17 01:26:28 +0100

    Ajout fichier test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("Salut");
+console.log("Faisons quelques calculs.");
+
+console.log(4 + 7);
+
+console.log(12 / 0);
+
+console.log("Au revoir !");
+
+var prenom =prompt("Votre prénom");
+alert( "bonjour "+prenom);
+
+if (3>0) {
+	console.log(3);
+}
